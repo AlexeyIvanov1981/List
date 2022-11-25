42f8bedd12dfcdeca863fd32729432306dae5b0c
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-11-25 21:34:03 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Main {
+    ArrayList<String> Sails = new ArrayList<>();
+    ArrayList<Integer> operations = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Привет");
+
+    }
+
+
+}
